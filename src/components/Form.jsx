4ea560a0dec88dfc43a7cd16c1862641d0a425fc
KingKaiZhuang <5b0g0024@stust.edu.tw>
2024-05-05 23:42:32 +0800

    chore: Refactor Form component to handle first and last name separately

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,26 @@
 import { useState } from "react";
 
 export default function Form() {
-  const [name, setName] = useState("");
+  const [name, setName] = useState({ firstname: "", lastname: "" });
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log(name);
+  };
   return (
     <div>
+      {name.firstname} - {name.lastname}
       <form>
         <input
-          onChange={(e) => setName(e.target.value)}
+          onChange={(e) => setName({ ...name, firstname: e.target.value })}
           type="text"
-          value={name}
+          value={name.firstname}
         />
+        <input
+          onChange={(e) => setName({ ...name, lastname: e.target.value })}
+          type="text"
+          value={name.lastname}
+        />
+        <button onClick={handleClick}>送出</button>
       </form>
     </div>
   );
